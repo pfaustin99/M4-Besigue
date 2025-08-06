b1c1f7389d88be0e2a408829e0437f4e5c8b64f1
pfaustin99 <62509493+pfaustin99@users.noreply.github.com>
2025-08-06 13:28:38 -0400

    Move GamePlayersLayoutView up by 30 points - change anchor point offset from -20 to -50 for all positions

--- M4-Besigue/Views/GameBoard2/Components/GamePlayersLayoutView.swift
+++ M4-Besigue/Views/GameBoard2/Components/GamePlayersLayoutView.swift
@@ -31,18 +31,20 @@ struct GamePlayersLayoutView: View {
                 .position(anchorPoint(for: position, in: geometry.size))
             }
         }
+        .frame(width: geometry.size.width, height: geometry.size.height)
+        .border(Color.red, width: 2) // DEBUG: Show players layout bounds
     }
 
     private func anchorPoint(for position: TablePosition, in size: CGSize) -> CGPoint {
         switch position {
         case .bottom:
-            return CGPoint(x: size.width / 2, y: size.height * 0.75 - 20)
+            return CGPoint(x: size.width / 2, y: size.height * 0.75 - 50)
         case .top:
-            return CGPoint(x: size.width / 2, y: size.height * 0.25 - 20)
+            return CGPoint(x: size.width / 2, y: size.height * 0.25 - 50)
         case .left:
-            return CGPoint(x: size.width * 0.25, y: size.height / 2 - 20)
+            return CGPoint(x: size.width * 0.25, y: size.height / 2 - 50)
         case .right:
-            return CGPoint(x: size.width * 0.75, y: size.height / 2 - 20)
+            return CGPoint(x: size.width * 0.75, y: size.height / 2 - 50)
         }
     }
 }
